64e426da7603d278def1894590d173d17422f97f
Turcan Ilya <ilieturcan424@gmail.com>
2025-10-09 11:21:28 +0300

    first commit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,51 @@
+class MarvelService {
+    _apiBase = "https://marvel-server-zeta.vercel.app/";
+    _apiKey = "apikey=d4eecb0c66dedbfae4eab45d312fc1df";
+    _baseOffset = 0;
+    _baseLimit = 9;
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    };
+
+    getAllCharacters = async (
+        offset = this._baseOffset,
+        limit = this._baseLimit
+    ) => {
+        const res = await this.getResource(
+            `${this._apiBase}characters?limit=${limit}&offset=${offset}&${this._apiKey}`
+        );
+
+        return res.data.results.map(this._transformCharacter);
+    };
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(
+            `${this._apiBase}characters/${id}?${this._apiKey}`
+        );
+
+        console.log(res);
+
+        return this._transformCharacter(res.data.results[0]);
+    };
+
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items,
+        };
+    };
+}
+
+export default MarvelService;
